42818f390443167a3c2ca5efc80d052c78b87f77
boni yheya <boniyehya@gmail.com>
2023-09-15 13:52:39 -0700

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -45,7 +45,7 @@ export default function Home() {
       <Layout>
         {/* Start Hero Section */}
         <Hero
-          title="Ennovix: Where Creativity <br/>Meets Technology"
+          title="Ennovix: Where Creativity <br/>Meets Technologyy"
           subtitle="We deliver best problem solving solution for our client and provide finest finishing product in present and upcoming future."
           btnText="Get Us"
           btnLink="/contact"
